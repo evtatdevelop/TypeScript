942c49c3a123a4b9250df791b1d55b845316c7fa
Evgenii <evtatdevelop@gmail.com>
2021-08-11 16:29:07 +0700

    TypeScript project of statistic

---
+++ stats/src/index.ts
@@ -0,0 +1,17 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResults";
+
+const csvFileReader = new CsvFileReader('football.csv');
+csvFileReader.read();
+
+let manUnitedWins = 0;
+
+for (let match of csvFileReader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} times.`);
